c658700fb4e7be7d908a3cf4efabee9c259bb16e
ChunWeiYeah <yher25@gmail.com>
2022-08-10 22:23:34 +0800

    新增作品集

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -3,47 +3,35 @@ import styled from "styled-components";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import WorksData from "../../helper/Works.json";
+import "../../Slick.css";
+
 const Works = () => {
   const settings = {
     dots: true,
-    infinite: true,
+    // infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    centerPadding: "500px",
+    // variableWidth: true,
   };
   return (
     <WorksStyled>
       <WorksName>作品集</WorksName>
 
-      <Slider {...settings} style={{ marginTop: "10px" }}>
-        <WorkCard>
-          <WorkCardText>1</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>2</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>3</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>4</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>5</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>6</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>7</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>8</WorkCardText>
-        </WorkCard>
-        <WorkCard>
-          <WorkCardText>9</WorkCardText>
-        </WorkCard>
+      <Slider {...settings} style={{ marginTop: "20px" }}>
+        {WorksData.map((work) => (
+          <a href={work.workUrl} target="_blank" rel="noreferrer">
+            <WorkCard>
+              <WordCardImage
+                src={`/images/${work.workIcon}`}
+                workIconWidth={work.workIconWidth}
+              />
+              <WorkCardText>{work.workName}</WorkCardText>
+              <WorkCardText>{work.workDesc}</WorkCardText>
+            </WorkCard>
+          </a>
+        ))}
       </Slider>
     </WorksStyled>
   );
@@ -52,7 +40,7 @@ const Works = () => {
 export default Works;
 
 const WorksStyled = styled.div`
-  height: 45vh;
+  height: 35vh;
   background-color: #393a3c;
 `;
 
@@ -63,21 +51,25 @@ const WorksName = styled.h1`
   text-align: center;
 `;
 
-const WorksCardContainer = styled.div`
-  margin-top: 15px;
-  display: flex;
-  flex-direction: row;
-`;
-
 const WorkCard = styled.div`
   background-color: #fff;
-  height: 18vh;
+  height: 20vh;
   /* width: 200px; */
   border-radius: 8px;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const WordCardImage = styled.img`
+  width: ${(prop) => prop.workIconWidth};
+  margin: 0 auto;
 `;
 
 const WorkCardText = styled.p`
   color: #000;
   font-size: 1.5em;
   text-align: center;
+  font-weight: bold;
+  text-decoration: none;
 `;
